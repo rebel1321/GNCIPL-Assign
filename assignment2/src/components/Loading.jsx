2dc71f3009d984030f271342f661856b61554e6f
Satyam <www.satyam200413@gmail.com>
2025-09-01 18:52:46 +0530

    Deleted Loading page

--- assignment2/src/components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-    return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-600"></div>
-        </div>
-    );
-};
-
-export default Loading;
