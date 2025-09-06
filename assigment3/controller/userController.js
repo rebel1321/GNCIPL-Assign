01da6b6f86ef476664713d70e5a602f87dc95fbb
Satyam <www.satyam200413@gmail.com>
2025-09-06 20:05:15 +0530

    Done some minor changes

--- assigment3/controller/userController.js
+++ assigment3/controller/userController.js
@@ -15,8 +15,7 @@ const registerUser = async (req, res) => {
         }
 
         // Hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         // Create user
         const user = await User.create({
