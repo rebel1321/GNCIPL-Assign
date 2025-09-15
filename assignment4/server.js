37bb1d7f24703f940890d2182485998fef0670c1
Satyam <www.satyam200413@gmail.com>
2025-09-15 14:46:21 +0530

    have some changes in server.js

--- assignment4/server.js
+++ assignment4/server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 
 connectDB();
 
-app.use('api/auth',authRoutes)
-app.use('api/leaves',leaveRoutes)
+app.use('/api/auth',authRoutes)
+app.use('/api/leaves',leaveRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
