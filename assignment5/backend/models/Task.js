83213099727a23285a4ad57f755a49a01c495bc9
Satyam <www.satyam200413@gmail.com>
2025-09-20 14:09:49 +0530

    Completed the backend of the task manager application for week 5

---
+++ assignment5/backend/models/Task.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true
+});
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
---
+++ assignment5/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+    <div className='bg-blue-500'>heylooooooo</div>
+  )
+}
+
+export default App
